58afd14264ab2861588b6ccd2a3232889bfd690d
Arcee05 <arcee1242@gmail.com>
2025-10-13 17:16:28 +0530

    Add Home Page Images

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import HeroText from "../src/components/HeroText"
+import ParalllaxBackground from "./components/ParallaxBackground"
 
 const Hero = () => {
     return (
         <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
-            <HeroText/>
+        <HeroText />
+        <ParalllaxBackground/>
       </section>
   )
 }
